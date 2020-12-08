332e2bedcfdf52bbd3bffae77947e660e1527113
Noah <noahcape@gmail.com>
2020-12-08 13:30:34 -0500

    finally working

--- src/functions/isLoggedIn.js
+++ src/functions/isLoggedIn.js
@@ -2,6 +2,7 @@
 // returns boolean 
 
 import axios from "axios"
+require("dotenv").config()
 
 const isLoggedIn = async () => {
     let token = localStorage.getItem("auth-token")
@@ -12,7 +13,7 @@ const isLoggedIn = async () => {
     }
 
     const tokenRes = await axios.post(
-        "http://localhost:5000/users/tokenIsValid",
+        `http://${process.env.REACT_APP_SERVER}/users/tokenIsValid`,
         null,
         { headers: { "x-auth-token": token } }
     )
